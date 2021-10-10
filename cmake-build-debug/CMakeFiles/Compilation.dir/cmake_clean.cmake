e054aa0646d2112ee15a110b7737ab5f5e073676
Priestell <43655831+Priestell@users.noreply.github.com>
2021-10-10 22:01:51 +0200

    Add files via upload
    Chargement du projet

---
+++ cmake-build-debug/CMakeFiles/Compilation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Compilation.dir/image.cpp.o"
+  "CMakeFiles/Compilation.dir/main.cpp.o"
+  "Compilation"
+  "Compilation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Compilation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/RayaT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayaT.dir/main.cpp.o"
+  "RayaT"
+  "RayaT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayaT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
